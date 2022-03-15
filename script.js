844e44cdc6a29a605311f307d25efb63400e2cdc
Bianca Blakesley <bianca.blakesley@gmail.com>
2022-03-15 14:24:20 -0400

    added 1 question to classwork

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// Write a function that takes in a number and prints out each number till n (the number inputted). Stop the loop when the index is 5.
+//1 Write a function that takes in a number and prints out each number till n (the number inputted). Stop the loop when the index is 5.
 
 
-// Write some code that console.log's the message "Do I have to repeat myself?" 1000 times. Use a loop but make sure not to get stuck in an infinite loop!
+//2 Write some code that console.log's the message "Do I have to repeat myself?" 1000 times. Use a loop but make sure not to get stuck in an infinite loop!
 
 
-// Write a function called countEven that takes a parameter n which is a whole number greater than or equal to 2.  When executed, countEven should console.log the even numbers from 2 to n.
+/**
+ * 3
+ * A pangram is a string that contains every letter of the alphabet
+ * Write a function that takes in a string parameter and returns true if the string is
+ * a pangram, otherwise return false. Ignore case 
+ */
+
+// Uncomment to test: 
+// console.log(isPangram('The quick brown fox jumps over the lazy dog'))
+// console.log(isPangram('I am not a pangram'))
+// console.log(isPangram('We promptly judged antique ivory buckles for the next prize'))
+// console.log(isPangram('We promptly judged antique ivory buckles for the prize'))
 
 
-// Imagine that each number in the numberArray below represents a person's age. Write a for...of loop that includes an if...else statement. If the element's value is under 18, print `The person is [#] years old. That is not old enough to vote.` to the console. If the element's value is 18 or more, print `This person can vote because they're [#] years old.` to the console. The [#] should be replaced by the value of the element in the array.
+
+//4 Imagine that each number in the numberArray below represents a person's age. Write a for...of loop that includes an if...else statement. If the element's value is under 18, print `The person is [#] years old. That is not old enough to vote.` to the console. If the element's value is 18 or more, print `This person can vote because they're [#] years old.` to the console. The [#] should be replaced by the value of the element in the array.
 
 let numberArray = [0, 3, 5, 7, 13, 17, 23, 33, 42, 55, 81, 89, 99];
 
